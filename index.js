81202f0d11de015a7f7a3bfb645f3ea40dcc4fb9
Ali Madooei <madooei@jhu.edu>
2020-10-13 11:09:20 -0400

    Step 3

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const express = require("express");
 const app = express();
 const port = 4567;
 
-const note = new Note("Sample Note", "Ali");
-console.log(note);
+const notes = [];
+notes.push(new Note("Sample 1", "Author 1"));
+notes.push(new Note("Sample 2", "Author 2"));
+notes.push(new Note("Sample 3", "Author 3"));
+console.log(notes);
 
 app.listen(port, () => {
   console.log(`Server is listening on http://localhost:${port}`);
